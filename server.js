7fa5e8bc6102287477bee8b608312a777efd4447
Eniola Sholate <teniolasholate1@gmail.com>
2025-10-23 08:17:29 +0100

    Initial commit for Sholate Motors website

---
+++ server.js
@@ -0,0 +1,18 @@
+// server.js
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static files from "public" folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// Default route
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`🚗 Sholate Motors is live on port ${PORT}`);
+});
